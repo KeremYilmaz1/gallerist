d1f59a59d9d1a7a541ad5e6a34ad209b5dfebf54
KeremYilmaz1 <ahmedkerem@gmail.com>
2026-09-11 15:40:15 +0300

    AppConfig ve SecurityConfig sınıfları eklendi

---
+++ src/main/java/com/KeremYilmaz/galerist/starter/config/AppConfig.java
@@ -0,0 +1,60 @@
+package com.KeremYilmaz.galerist.starter.config;
+
+import com.KeremYilmaz.galerist.starter.entity.User;
+import com.KeremYilmaz.galerist.starter.exception.BaseException;
+import com.KeremYilmaz.galerist.starter.exception.ErrorMessage;
+import com.KeremYilmaz.galerist.starter.exception.MessageType;
+import com.KeremYilmaz.galerist.starter.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+@Configuration
+public class AppConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                Optional<User> optionalUser = userRepository.findByUsername(username);
+                if(optionalUser.isEmpty()){
+                    //Hata Fırlat
+                    throw new BaseException(new ErrorMessage(MessageType.USERNAME_NOT_FOUND , username));
+                }
+                return optionalUser.get();
+            }
+        };
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+}
--- src/main/java/com/KeremYilmaz/galerist/starter/exception/MessageType.java
+++ src/main/java/com/KeremYilmaz/galerist/starter/exception/MessageType.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum MessageType {
     NO_RECORD_EXIST("1004" , "kayıt bulunamadı"),
-    TOKEN_IS_EXPIRED("1005" , "token is expired"),
+    TOKEN_IS_EXPIRED("1005" , "token'in süresi bitmiştir"),
+    USERNAME_NOT_FOUND("1006" , "username bulunamadı"),
     GENERAL_EXCEPTION("9999" , "Genel bir hata oluştu");
 
     private String code;
